57c4ca7bdfac8b5f493985e4cf34e507dde79377
DJ-Jeson <jsun47@me.com>
2018-12-20 19:16:28 +0800

    v1.1

--- js/signin_add.js
+++ js/signin_add.js
@@ -1,20 +1,53 @@
-window.onload = function(){
+window.onload = function() {
+  // 登陆页
+  if ($("#loginPage").length > 0) {
+    $("form.signinForm").on("submit", function(e) {
+      _form = $(this);
 
-    var signinForm = function(){
+      if (_form[0].checkValidity() === false) {
+        e.preventDefault();
+        e.stopPropagation();
+      }
+      _form.addClass("was-validated");
+    });
+  }
 
-    }
+  // 主页面
+  if ($("#mainPage").length > 0) {
+    $("button.goBack").on("click", function(e) {
+      location.href = "index.html";
+    });
 
-    $('form.signinForm').on("submit", function(e){
+    $("#eventList ul li").on("click", function(e) {
+      _this = $(this);
 
-        _form = $(this);
+      // 未开始
+      if (!_this.hasClass("worked") && !_this.hasClass("completed")) {
+        // 待训练
+        _this
+          .addClass("working")
+          .find(".btn")
+          .attr("disabled", true)
+          .text("进行中");
 
-        if(_form[0].checkValidity()===false) {
-            e.preventDefault();
-            e.stopPropagation();
-        }
-        if($("#userID").val() == "") {
-            _form.addClass('invalid')
-        }
-        _form.addClass('was-validated');
-    })
-}
+        var timeout = setTimeout(function() {
+          _this
+            .removeClass("working")
+            .addClass("worked")
+            .find(".btn")
+            .removeClass("disabled")
+            .text("点击完成训练");
+        }, 10000);
+      }
+      // 进行中
+      if (_this.hasClass("working")) {
+        return false;
+      }
+      // 待完成
+      if (_this.hasClass("worked") && !_this.hasClass("completed")) {
+        _this.removeClass("worked").addClass("completed");
+        _this.find(".btn").text("已完成");
+      }
+    });
+  }
+};
